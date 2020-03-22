6f56f959f469b3088832ab8bd195e27044484c80
A-Chesnais <56262220+A-Chesnais@users.noreply.github.com>
2020-03-22 12:29:50 +0100

    Update functions.py

--- Repertoire de travail/functions.py
+++ Repertoire de travail/functions.py
@@ -14,6 +14,8 @@ from scipy.sparse import csr_matrix
 from wordcloud import WordCloud
 from IPython.display import display
 
+# Fonction permettant d'obtenir la frequence de chacun des mots
+# au sein de l'ensemble des questions
 def words_freq(questions):
     # Initialisation du counter, fitting du modèle au données et transformation de celles ci
     count_vectorizer = CountVectorizer(stop_words=None)
@@ -38,6 +40,8 @@ def words_freq(questions):
     plt.axis("off")
     return df_words_count
 
+# Fonction permettant d'obtenir la frequence de chacun des tags
+# associes a l'ensemble des questions
 def tags_freq(tags, vectorizer):
     # fitting du modèle au données et transformation de celles ci
     sparse_tags = vectorizer.fit_transform(tags)
@@ -61,17 +65,30 @@ def tags_freq(tags, vectorizer):
     plt.axis("off")
     return df_tags_count, sparse_tags
 
+# Fonction permettant d'identifier les questions qui possedent au moins un tag
+# parmi les X plus representes
 def tags_questions_coverage(sparse, tags_frequencies, max_tags, min_tags):
-    '''
-    '''
+    
+    # Selection de X tags les plus importants (max_tags)
     tags_to_select = list(tags_frequencies[:max_tags].index)
+    
+    # Reduction de la matrice questions / tags au X tags retenus
     reduced_sparce = sparse[:,tags_to_select]
+    
+    # Calcul du % de questions qui possedent toujours au moins un tag
+    # dans la matrice questions / tags reduites
     questions_coverage = reduced_sparce[reduced_sparce.getnnz(1)>(min_tags-1)].shape[0] / sparse.shape[0] * 100
+    
     print('{:03.2f}'.format(questions_coverage) + ' questions have at least ' + str(min_tags) + ' tag(s)')
+    
+    # Renvoi du taux de couverture et la matrice questions / tags conservant
+    # uniquement les questions qui ont toujours au moins un tag
     return questions_coverage, reduced_sparce.getnnz(1)>(min_tags-1);
 
-
+# Fonction permettant d'afficher les mots les plus importants
+# par sujet d'un modele LDA ou NMF
 def print_top_words(model, feature_names, n_top_words):
+    
     for topic_idx, topic in enumerate(model.components_):
         message = "Topic #%d: " % topic_idx
         message += " ".join([feature_names[i]
@@ -79,57 +96,90 @@ def print_top_words(model, feature_names, n_top_words):
         print(message)
     print()
 
+# Fonction permettant de collecter les mots les plus importants
+# par sujet d'un modele LDA ou NMF et de les retenir s'ils font partie
+# d'une liste de tags communs
 def get_top_words(model, model_type, feature_names, n_top_words, tags_list):
     
     results =pd.DataFrame(columns = ['topic', 'key_words'])
     
+    # Choix du criteres d'importances d'un mots selon le type de modele
     if model_type == 'lda':
         model_components = model.components_ / model.components_.sum(axis=1)[:, np.newaxis]
     else:
         model_components = model.components_
     
+    # Selection des mots les plus importants et de leur poids par sujet du modele
     for topic_idx, topic in enumerate(model_components):
         results = results.append({'topic': topic_idx,
                                   'key_words': dict(zip([feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]],
                                   topic[topic.argsort()[:-n_top_words - 1:-1]]))}, ignore_index=True)
         
+    # Etape de filtrage en croisant importances des mots et liste de tags communs
     results['potential_tags'] = results['key_words'].apply(lambda x: [(keyword,weight) for (keyword,weight) in x.items() if keyword in tags_list])
 
     results['potential_tags_weighted'] = results['potential_tags'].apply(lambda x: dict(x))
     
+    # Affichage du nombre total de tags potentiels obtenus
     print('Total number of tags :')
     tags_number = results['potential_tags_weighted'].apply(lambda x: list(x.keys()))
     print(len(set(tags_number.sum())))
     
     results.drop(labels=['potential_tags'], axis=1, inplace=True)
     
+    # Affichage des resultats
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):
         display(results)
         
     return results
 
+# Fonction permettant de sauvegarder un modele non supervisee type LDA ou NMF
+# au format json
 def save_unsupervivised_model(features, vectorizer, target, potential_tags , filename):
     
+    # Creation d'un dictionnaire vide
     model = {}
+    
+    # Ajout du Bag of Word sous forme de dictionnaire 
+    # (conversion des proprietes de la matrice creuse questions / features)
     model['features'] = {'data': features.data.tolist(), 'indices': features.indices.tolist(), 
                          'indptr': features.indptr.tolist(), 'shape': features.shape}
+    
+    # Ajout du mapping numero de features / mot associe
     model['vocabulary'] = dict([key, value.item()] for key, value in vectorizer.vocabulary_.items())
+    
+    # Ajout de la matrice exprimant chaque question en fonction des differents sujets 
     model['target'] = target.tolist()
+    
+    # Ajout de la liste des tags potentiels et leur poids associes à chaque sujet
     model['topic_tags'] = potential_tags['potential_tags_weighted'].to_dict()
     
+    # Sauvergade au format json
     json_txt = json.dumps(model)
     with open(filename, 'w') as file:
         file.write(json_txt)
 
+
+# Fonction permettant de charger un modele non supervisee type LDA ou NMF
+# à partir d'un fichier json
 def load_unsupervivised_model(filename):
     
+    # Ouverture du fichier
     with open(filename, 'r') as file:
         model = json.load(file)
         
+    # Creation de la matrice creuse des features sous forme de Bag Of Word
     features = csr_matrix((np.asarray(model['features']['data']), np.asarray(model['features']['indices']), 
                            np.asarray(model['features']['indptr'])), shape=model['features']['shape'])
+    
+    # Chargement du mapping numero de features / mot associe
     vocabulary = model['vocabulary']
+    
+    # Reconstruction de la matrice exprimant chaque question en fonction des 
+    # differents sujets sous forme de tableau Numpy
     target = np.asarray(model['target'])
+    
+    # Chargement de la liste des tags potentiels et leur poids associes à chaque sujet
     tags = model['topic_tags']
     
     return features, target, vocabulary, tags
